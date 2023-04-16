30013f08abfccbe9ba6535a848bf6569307de245
rohitg247 <106257406+rohitg247@users.noreply.github.com>
2023-04-16 19:14:55 +0530

    second commit

---
+++ components/Navbar.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { useRouter } from 'next/dist/client/router';
+
+const Navbar = () => {
+    const router = useRouter();
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar onClick={() => router.push('/')}>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Pokemon
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+};
+
+export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import { Box } from '@mui/material'
+import Navbar from '../components/Navbar'
+// import '../styles/global.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Box style={{
+        backgroundImage: 'url(https://w0.peakpx.com/wallpaper/433/219/HD-wallpaper-pokeball-nintendo-pocket-monsters-pokemon.jpg)', backgroundRepeat: "no-repeat",
+        backgroundSize: "cover"
+      }} >
+        <Component {...pageProps} />
+      </Box>
+    </>
+  )
 }
